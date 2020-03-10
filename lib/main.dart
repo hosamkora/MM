bba6341488b00ad2fd4d2316520c9cd42bd9fe77
Hosam Hasan <hosamkora@gmail.com>
2020-03-10 23:24:30 +0200

    list view test

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
-import 'package:hosam_test/view_model.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/services.dart';
 
 void main(List<String> args) {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) => ListViewModel(),
-      child: Home(),
-    ),
-  );
+  runApp(Home());
 }
 
 class Home extends StatelessWidget {
@@ -17,78 +13,152 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SafeArea(
-        child: Content(),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Body(),
     );
   }
 }
 
-class Content extends StatelessWidget {
-  Content({Key key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final controller = TextEditingController();
-    return Scaffold(
-      appBar: AppBar(),
-      body: Body(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: () {
-          final model = Provider.of<ListViewModel>(context, listen: false);
-          showDialog(
-            context: context,
-            builder: (_) {
-              return Center(
-                child: Container(
-                  color: Colors.grey,
-                  width: 300,
-                  height: 100,
-                  child: Material(
-                      child: Column(
-                    children: <Widget>[
-                      TextField(
-                        controller: controller,
-                      ),
-                      FlatButton(
-                        onPressed: () {
-                          final model = Provider.of<ListViewModel>(context,
-                              listen: false);
-                          final val = controller.text;
-                          model.additem(val);
-                          Navigator.of(context).pop();
-                        },
-                        child: Text('Done'),
-                      )
-                    ],
-                  )),
-                ),
-              );
-            },
-          );
-        },
-      ),
-    );
-  }
-}
-
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
   const Body({Key key}) : super(key: key);
 
   @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  var list = <ItemData>[];
+
+  @override
   Widget build(BuildContext context) {
-    return Consumer<ListViewModel>(
-      builder: (_, model, __) {
-        return ListView(
-          children: model.strings
-              .map((string) => Text(
-                    string,
-                    style: TextStyle(fontSize: 35),
-                  ))
-              .toList(),
-        );
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Things"),
+        centerTitle: true,
+        actions: <Widget>[
+          Icon(Icons.shopping_cart),
+          Text(list.length.toString())
+        ],
+      ),
+      body: ListView(
+        children: list.map((itemData) => CustomListItem(itemData)).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          addItem();
+        },
+        child: Icon(Icons.add),
+      ),
     );
   }
+
+  void addItem() {
+    final nameController = TextEditingController();
+    final ageController = TextEditingController();
+    showDialog(
+        context: context,
+        builder: (_) {
+          return Center(
+            child: Material(
+              color: Colors.transparent,
+              child: Container(
+                padding: EdgeInsets.all(16),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueAccent, width: 5),
+                  borderRadius: BorderRadius.circular(15),
+                  color: Colors.white,
+                ),
+                width: 300,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(labelText: "Name"),
+                    ),
+                    TextField(
+                      controller: ageController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [
+                        WhitelistingTextInputFormatter.digitsOnly
+                      ],
+                      decoration: InputDecoration(labelText: "age"),
+                    ),
+                    FlatButton(
+                      child: Text("Confirm"),
+                      onPressed: () {
+                        final name = nameController.text;
+                        final age = int.parse(ageController.text);
+
+                        final width = Random().nextInt(500);
+                        list.add(ItemData(
+                            name: name,
+                            age: age,
+                            imageUrl:
+                                "https://placeimg.com/$width/$width/any"));
+                        setState(() {});
+                        Navigator.of(context).pop();
+                      },
+                    )
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
+
+// https://placeimg.com/400/400/any
+
+class CustomListItem extends StatelessWidget {
+  final ItemData itemData;
+  const CustomListItem(this.itemData, {Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 100,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: <Widget>[
+            Image(
+              image: NetworkImage(itemData.imageUrl),
+              height: 100,
+            ),
+            SizedBox(width: 10),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  itemData.name,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  "${itemData.age} year",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ItemData {
+  final String name;
+  final int age;
+  final String imageUrl;
+
+  ItemData({this.name, this.age, this.imageUrl});
 }
